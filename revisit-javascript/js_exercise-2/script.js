68ff73389a20d0ba033da12b63542f05184714ef
DebbyMurphy <debby.m.jones@gmail.com>
2020-09-11 14:27:42 +0800

    Updated HTML & JS files for Exercise 2

--- revisit-javascript/js_exercise-2/script.js
+++ revisit-javascript/js_exercise-2/script.js
@@ -1,19 +1,6 @@
-let carts = document.querySelectorAll('.add-cart');
-
-for (let i=0; i < carts.length; i++) {
-    carts[i].addEventListener('click', () => {
-        console.log("added to cart");
-    })
-}
+let cart = []
 
 
---------------------------------------------------------------------------------------
-
-let product-name = "Product Name"
-
-function addProductName() {
-    // var count = document.getElementById('number');
-    // number--;
-    // count.innerHTML = number;
-    return product-name
+function addToCart() {
+    pass
 }
